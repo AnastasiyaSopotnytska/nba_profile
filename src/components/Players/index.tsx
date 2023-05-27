b09172e2618cf0a6e97b0398bd40893bb9afebc4
Anastasiia Sopotnytska <nastyasopotnytska14@gmail.com>
2023-05-27 14:54:26 +0300

    create teams and players files

---
+++ src/components/Players/index.tsx
@@ -0,0 +1,7 @@
+export const Players = () => {
+  return (
+    <div className="mt-20">
+      <h2 className="text-main font-bold text-3xl">Players</h2>
+    </div>
+  )
+}
---
+++ src/components/Teams/index.tsx
@@ -0,0 +1,9 @@
+export const Teams = () => {
+  return (
+    <div className="mt-10 flex flex-col gap-6">
+      <h2 className="text-main font-bold text-3xl">
+        Teams
+      </h2>
+    </div>
+  )
+}
